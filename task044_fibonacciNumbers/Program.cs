5db0b10cad38123502a7f1ad6bb17bb6247801fa
maxim_stoma <maksim.stoma@mail.ru>
2022-05-29 17:51:18 +0300

    task044 solved

---
+++ task044_fibonacciNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿void FibonacciSequence(int[] array)
+{
+    array[0] = 0;
+    array[1] = 1;
+    Console.Write(", ");
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 2] + array[i - 1];
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(".");
+}
+
+Console.WriteLine("Сколько чисел последовательности Фибоначчи вывести?");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] fibonacci = new int[n];
+fibonacci[1] = 1;
+Console.Write($"Последовательность Фибоначчи до {n}: {fibonacci[0]}, {fibonacci[1]}");
+
+FibonacciSequence(fibonacci);
+Console.ReadLine();
