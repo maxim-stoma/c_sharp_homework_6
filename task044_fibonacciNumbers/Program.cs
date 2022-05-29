d27216f05aee45e2e8c24a6e57b30296c5335f9a
maxim_stoma <maksim.stoma@mail.ru>
2022-05-29 18:00:05 +0300

    task044 edited (solved problem with coma after second number while printin' only two numbers

--- task044_fibonacciNumbers/Program.cs
+++ task044_fibonacciNumbers/Program.cs
@@ -2,7 +2,7 @@
 {
     array[0] = 0;
     array[1] = 1;
-    Console.Write(", ");
+    
     for (int i = 2; i < array.Length; i++)
     {
         array[i] = array[i - 2] + array[i - 1];
@@ -20,6 +20,10 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[] fibonacci = new int[n];
 fibonacci[1] = 1;
 Console.Write($"Последовательность Фибоначчи до {n}: {fibonacci[0]}, {fibonacci[1]}");
+if (n > 2)
+{
+    Console.Write(", ");
+}
 
 FibonacciSequence(fibonacci);
 Console.ReadLine();
