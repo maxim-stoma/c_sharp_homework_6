2463dac7445e6b02bac740c61fc5f8f8c7e4c5bb
maxim_stoma <maksim.stoma@mail.ru>
2022-05-30 21:15:01 +0300

    task044-1 solved

---
+++ task044-1_fibonacciNumbersSecondWay/Program.cs
@@ -0,0 +1,40 @@
+﻿void FibonacciSequence()
+        {
+            Console.WriteLine("Сколько чисел последовательности Фибоначчи надо?");
+            int howMany = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Последовательность Фибоначчи: ");
+            
+            int result;
+            int firstNumber = 0;
+            int secondNumber = 1;
+            if (howMany <= 1)
+            {
+                Console.Write(firstNumber);
+            }
+            else if (howMany == 2)
+            {
+                Console.Write(firstNumber + ", ");
+                Console.Write(secondNumber);
+            }
+            else
+            {
+                Console.Write(firstNumber + ", ");
+                Console.Write(secondNumber + ", ");
+                for (int i = 3; i <= howMany; i++)
+                {
+                    result = firstNumber + secondNumber;
+                    Console.Write(result);
+                    firstNumber = secondNumber;
+                    secondNumber = result;
+                    if (i != howMany)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+            Console.Write(".");
+        }
+        
+        FibonacciSequence();
+        Console.ReadLine();
