fd77b5ea14108b15b25a4de60fa89b4d3a7d357b
maxim_stoma <maksim.stoma@mail.ru>
2022-05-30 21:07:21 +0300

    task044 edited (solved problem with printin' zero, one and two numbers of sequence) - solved

--- task044_fibonacciNumbers/Program.cs
+++ task044_fibonacciNumbers/Program.cs
@@ -1,29 +1,47 @@
 ﻿void FibonacciSequence(int[] array)
-{
-    array[0] = 0;
-    array[1] = 1;
-    
-    for (int i = 2; i < array.Length; i++)
-    {
-        array[i] = array[i - 2] + array[i - 1];
-        Console.Write(array[i]);
-        if (i != array.Length - 1)
         {
-            Console.Write(", ");
+            if(array.Length < 1)
+            {
+                Console.WriteLine("So what do you want to see here?");
+            }
+            
+            else if (array.Length == 1)
+            {
+                array[0] = 0;
+                Console.Write(array[0]);
+            }
+            else
+            {
+                for (int index = 0; index < 2; index++)
+                {
+                    array[index] = index;
+                    Console.Write(array[index]);
+                    if (index != 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                if (array.Length > 2)
+                {
+                    Console.Write(", ");
+                }
+                for (int i = 2; i < array.Length; i++)
+                {
+                    array[i] = array[i - 2] + array[i - 1];
+                    Console.Write(array[i]);
+                    if (i != array.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.Write(".");
+            }    
         }
-    }
-    Console.Write(".");
-}
-
-Console.WriteLine("Сколько чисел последовательности Фибоначчи вывести?");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] fibonacci = new int[n];
-fibonacci[1] = 1;
-Console.Write($"Последовательность Фибоначчи до {n}: {fibonacci[0]}, {fibonacci[1]}");
-if (n > 2)
-{
-    Console.Write(", ");
-}
-
-FibonacciSequence(fibonacci);
-Console.ReadLine();
+            
+        Console.WriteLine("How many Fibonacci numbers do you want to print?");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] fibonacci = new int[n];
+        Console.Write("Fibonacci numbers from 1 to " + n + ": ");
+        
+        FibonacciSequence(fibonacci);
+        Console.ReadLine();
